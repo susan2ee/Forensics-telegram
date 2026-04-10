3a2b0da3f3473c826235859ee9aeccc6bcdb33d4
Song2 <161666101+susan2ee@users.noreply.github.com>
2026-04-10 15:51:58 +0900

    Hide contents

--- Automation Script/automation_script_K.py
+++ Automation Script/automation_script_K.py
@@ -1,1 +1,28 @@
+################################
+# Close Tab State Functions
+
+# Helper Functions
+
+# Deleting Message
+
+# Scheduling Message
+
+# Deleting Scheduled Message
+
+# Editing Message
+
+# Leaving / Deleting Chats
+
+# Memory Dumping
+
+# Shutdown Virtual Machine
+
+# Creating Chat / Group / Channel (Web K)
+
+# Promoting User to Channel Admin (Web K)
+
+# Main Loop
+
+################################
+
 
